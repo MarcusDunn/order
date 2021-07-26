06d8cbea64d05dfac63da26594321be7cac60818
marcusdunn <marcus.s.dunn@gmail.com>
2021-07-26 16:52:34 -0700

    futher tests and parser fixes

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,11 @@ mod tests {
 
     #[test]
     fn test_number_literal1() {
+        OrderParser::parse(Rule::number, "1 ").unwrap();
+    }
+
+    #[test]
+    fn test_number_literal2() {
         OrderParser::parse(Rule::number, "-123_122").unwrap();
     }
 
@@ -27,6 +32,11 @@ mod tests {
     #[test]
     fn test_literal2() {
         OrderParser::parse(Rule::literal, "-123_122").unwrap();
+    }
+
+    #[test]
+    fn test_literal3() {
+        OrderParser::parse(Rule::literal, "1").unwrap();
     }
 
     #[test]
@@ -97,6 +107,11 @@ mod tests {
 
     #[test]
     fn test_function_definition1() {
-        OrderParser::parse(Rule::functionDefinition, "one :: Int\nid a = a\n\n").unwrap();
+        OrderParser::parse(Rule::functionDefinition, "id :: Int -> Int\nid a = a\n\n").unwrap();
+    }
+
+    #[test]
+    fn test_function_definition2() {
+        OrderParser::parse(Rule::functionDefinition, "one :: Int\none = 1\n\n").unwrap();
     }
 }
