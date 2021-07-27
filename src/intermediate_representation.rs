df9b373ea521831e6d33a57adba85616913c9dea
marcusdunn <marcus.s.dunn@gmail.com>
2021-07-26 22:28:58 -0700

    started taking a stab at implementing the type system

--- src/intermediate_representation.rs
+++ src/intermediate_representation.rs
@@ -1,13 +1,36 @@
-struct Context {
+pub(crate) struct Context {
     types: Vec<Type>,
 }
 
 enum Type {
-    Universe(u128),
+    /// universe type
+    Type,
     Atom(AtomicType),
 }
 
-struct AtomicType {
-    type_id: u128,
-    name: String,
+
+enum AtomicType {
+    BinaryProductType(BinaryProductType),
+    DependentProductType(DependentProductType),
+    Unit,
+}
+
+struct BinaryProductType {
+    left: Box<Type>,
+    right: Box<Type>,
+}
+
+
+struct DependentProductType {
+    dependant: Box<Term>,
+    dependee: Box<Type>,
+}
+
+struct Term {}
+
+impl DependentProductType {
+    // make sure to typecheck the term
+    fn new(context: &Context, family: Box<dyn Fn(Term) -> Type>, term: Term) -> Result<Self, &'static str> {
+        todo!()
+    }
 }
